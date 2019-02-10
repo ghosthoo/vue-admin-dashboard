29a670005d775df963d702aea76cf27abea4c786
Scott Thoo <1thoo3@gmail.com>
2019-02-10 23:40:29 +0800

    Dark Mode 1 and 2
    Dark Mode 1
    Dark Mode 2

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,40 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
+<script>
+export default {
+  name: "App",
+  mounted() {
+    const isDarkMode = this.$store.getters.isDarkMode;
+    document.body.style.background = isDarkMode ? "#212c4f" : "#f0f3f5";
+  }
+};
+</script>
+
 <style lang="scss">
+body {
+  background: $dark-blue;
+}
+
+h1 {
+  @include heading-1;
+}
+
+p {
+  @include large-text-bold($purple);
+}
+
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: $system-font-family;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $white;
 }
+
 #nav {
   padding: 30px;
   a {
---
+++ src/views/SignInFlow/ABC.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>SDSD</div>
+</template>
+
+<script>
+export default {
+  name: "ABC"
+};
+</script>
+
+<style scoped>
+</style>
